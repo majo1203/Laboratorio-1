a3312b65cc327cbe5ad433c073da63ea2ffebdc8
Maria Jose Cabre <47464020+majo1203@users.noreply.github.com>
2019-03-25 19:42:12 -0300

    clase de funciones

---
+++ Clase_de_funciones/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int suma(int op1, int op2)
+{
+    int res;
+    res= op1 + op2;
+    return res;
+}
+int main()
+{
+    int n1, n2, result;
+    printf("Numero 1: ");
+    scanf("%d", & n1);
+    printf("Numero 2: ");
+    scanf("%d", & n2);
+    result= suma(n1, n2);
+    printf("La suma es: %d", result);
+    return 0;
+}
