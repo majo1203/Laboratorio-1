f5bc163d3115b59e97a969113ea43111feb3ade1
Maria Jose Cabre <47464020+majo1203@users.noreply.github.com>
2019-03-25 22:10:43 -0300

    Clase 3 pruebas

---
+++ Prueba1/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void carga(void);
+void main (void)
+{
+    int x, y, z;
+    var=5;
+    carga();
+    printf("%d",var);
+}
+void carga(void)
+{
+    int var;
+    var=3;
+}
